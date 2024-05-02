ad6181ac0c90cb99f5ee96557a6834cfcf74483e
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-05-02 15:33:57 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,32 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-
-
-
-
-
-
-
-
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
 let color;
 const button = document.querySelector(".change-color");
 const body = document.querySelector("body");
 const span = document.querySelector(".color");
-
 const handleClick = () => {
   color = getRandomHexColor();
   console.log(color);
   body.style.backgroundColor = color;
   span.textContent = color;
 };
-
 button.addEventListener("click", handleClick);
