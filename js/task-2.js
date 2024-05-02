41923eb09b49fb182bdc23375ff99e5b7a3d5a48
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-05-02 14:51:12 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,12 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+const gallery = document.querySelector('.gallery');
+const pairs = [];
+for (let i = 0; i < images.length; i += 2) {
+    pairs.push(images.slice(i, i + 2));
+}
+
+const imagesMarkup = pairs.map(pair => 
+    `<li>${pair.map(({ url, alt }) => `<img src="${url}" alt="${alt}">`).join('')}</li>`
+).join('');gallery.insertAdjacentHTML('afterbegin', imagesMarkup);
