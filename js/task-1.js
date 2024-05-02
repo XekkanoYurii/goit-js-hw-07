4c5937c90f081266fe530bd8b58c5e477f710782
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-05-02 15:33:51 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,9 @@
 function getCategories() {
     const categories = document.querySelectorAll(".item");
-
     console.log(`Number of categories: ${categories.length}`);
     categories.forEach(category => {
         console.log(`Category: ${category.querySelector("h2").textContent}`);
         console.log(`Elements: ${category.querySelectorAll("li").length}`);
     });
 }
-
 getCategories();
